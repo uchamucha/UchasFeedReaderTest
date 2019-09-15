cec4df463b8e1b7f01ddb6cf871c67172757ed3e
Ucha <usamathpallikkachalil@gmail.com>
2019-09-15 16:30:39 +0530

    reworked code. added comments.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -66,12 +66,12 @@ $(function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-         /* TODO: Write a test that ensures the menu changes
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-         it('clicking menu changes visibility', function(){
+        it('clicking menu changes visibility', function(){
             $('a.menu-icon-link').click();//trigger click
             expect($('body').hasClass('menu-hidden')).toBe(false);
             $('a.menu-icon-link').click();//trigger click again
@@ -88,19 +88,19 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-         beforeEach(function(done){
-             loadFeed(0, function(){
-                 done();
-             });
-         });
-
-         it('one element exists', function(){
-             expect($('.feed .entry').length).not.toBe(0);
-         })
-
-
-
+        beforeEach(function(done){
+            //load zeroth feed
+            loadFeed(0, function(){
+               done();
+            });
+        });
+         //check if atleast an element exists in its entry
+        it('one element exists', function(){
+            expect($('.feed .entry').length).not.toBe(0);
+        })
     });
+
+
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function(){
         /* TODO: Write a test that ensures when a new feed is loaded
@@ -110,11 +110,11 @@ $(function() {
         let feed0;
 
         beforeEach(function(done){
-            //load 0th feed and store it
+            //load zeroth feed and store it
             loadFeed(0, function(){
                 feed0 = $('.feed').html();
                 //load next feed to test it against old feed in it statement
-                loadFeed(1, function(done){
+                loadFeed(1, function(){
                     done();
                 });
             })
